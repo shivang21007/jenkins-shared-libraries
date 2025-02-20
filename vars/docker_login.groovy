8904cfe0b955bf8d9d21fa3dfab42b90aa75c95f
Shivang Gupta <shivanggupta2611@gmail.com>
2025-02-20 18:19:04 +0530

    Update docker_login.groovy

--- vars/docker_login.groovy
+++ vars/docker_login.groovy
@@ -1,3 +1,13 @@
-def call(){
-
+def call(String Project_Name, String Version_tag){
+   echo "******** Pushing to Docker hub **********"
+                withCredentials([usernamePassword(
+                    credentialsId:"dockerHubCred",
+                    usernameVariable:"dockerHubUser", 
+                    passwordVariable:"dockerHubPass")]){
+                  
+                    sh 'echo $dockerHubPass | docker login -u $dockerHubUser --password-stdin'
+                    sh "docker image tag ${Project_Name}:${Version_tag} $dockerHubUser/${Project_Name}:${Version_tag}"
+                    sh "docker push $dockerHubUser/${Project_Name}:${Version_tag}"
+                }
+  echo "******** Successfully Pushed to Docker hub **********"
 }
